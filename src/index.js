6c1cca38634e4df08c50e9f3455f533604d63d94
Myat-Theingi-Aung <myattheingiaung1720@gmail.com>
2023-09-07 15:16:31 +0630

    add fontawesome icon

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import reportWebVitals from './reportWebVitals';
 import './scss/custom.scss';
 import bootstrap from 'bootstrap';
 import { BrowserRouter } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +16,7 @@ root.render(
     <App />
   </BrowserRouter>
 );
-
+library.add(fab, fas, far)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
