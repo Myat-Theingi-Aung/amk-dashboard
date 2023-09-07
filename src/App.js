85697558482d64491e339a5cb776b076a60855da
Myat-Theingi-Aung <myattheingiaung1720@gmail.com>
2023-09-07 10:30:51 +0630

    add bootstrap jquery sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import $ from 'jquery';
+
 function App() {
+  const jQueryCode = () => {
+    $('h1').css('color', 'red')
+  }
+
   return (
     <>
+      <h1 className="text-center">App</h1>
+      <button className="btn btn-primary" onClick={jQueryCode}>click</button>
     </>
   );
 }
